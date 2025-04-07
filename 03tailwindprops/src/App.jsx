8a55af272afde36554e05509ed1b9ddd70b97b86
Isha Shrivastava <isha8801@gmail.com>
2025-04-07 17:24:22 +0530

    03tailwindprops

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,15 +1,21 @@
-import {  useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import React from 'react'
 import './App.css'
+import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  let myObj = {
+    userName :'Isha',
+    age:23
+  }
   return (
     <>
-      <h1 className='bg-blue-950 p-4 rounded-2xl'>Tailwind test</h1>
+      <h1 className='bg-green-200 p-5 rounded-2xl mb-5'>Tailwind test</h1>
+  
+     <Card userName ="Isha" btnText ="Click Me"/>
+     <Card userName ="Isha" />
     </>
   )
 }
